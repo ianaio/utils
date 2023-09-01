5fdfb1e042bc527bf62e6239d514f7724f41af11
ianaio <dev@iana.io>
2023-09-01 16:59:25 +0200

    library utilities

---
+++ lib/regExpUtils.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) IANA SYSTEMS, Inc. and its affiliates.
+ *
+ * This source code is licensed under the IANA SYSTEMS license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+export declare function escapeRegexp(string: string): string;
+//# sourceMappingURL=regExpUtils.d.ts.map
+
---
+++ lib/shellUtils.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) IANA SYSTEMS, Inc. and its affiliates.
+ *
+ * This source code is licensed under the IANA SYSTEMS license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+export declare function escapeShellArg(s: string): string;
+//# sourceMappingURL=shellUtils.d.ts.map
+
